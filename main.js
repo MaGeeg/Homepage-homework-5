5a7821377fa636e1c80cf839f9fb523050272bae
Grzegorz Zielinski <gregoryblack11@gmail.com>
2020-04-11 14:39:09 +0200

    feature:stworzenie nawigacji wersja BEM i uzupełnienie el. za pomocą JS

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 console.log('Wtaj w digitalowym Świecie');
 console.log(`Remember: Every Journey starts with a single line of code`);
+
+const para = document.querySelector('.paragraph');
+console.log(para.innerHTML);
+para.innerHTML = 'Witaj Piotr';
+console.log(para.innerHTML);
+para = document.querySelector('.article__Header');
+console.log(para.innerHTML);
+para.innerHTML = 'Pijany poniedziałek';
