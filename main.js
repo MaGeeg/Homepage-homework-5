d0c5d9d487d72cfbf78b4b74d06e78d4b765cd87
Grzegorz Zielinski <gregoryblack11@gmail.com>
2020-04-11 18:51:46 +0200

    feature: fixed paragraph class name

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 console.log('Wtaj w digitalowym Świecie');
 console.log(`Remember: Every Journey starts with a single line of code`);
 
-const para = document.querySelector('.paragraph');
+const para = document.querySelector('.paragraph--js');
 console.log(para.innerHTML);
 para.innerHTML = 'Witaj Piotr';
 console.log(para.innerHTML);
-para = document.querySelector('.article__Header');
-console.log(para.innerHTML);
-para.innerHTML = 'Pijany poniedziałek';
